f4421821e8af9dec525756babacf23e3634d0391
SaltedSaltedFish <34928887+SaltedSaltedFish@users.noreply.github.com>
2019-02-21 11:34:23 +0800

    删除web文件夹，更新学习进度

--- 第一个python程序/hello.py
+++ 第一个python程序/hello.py
@@ -1,5 +1,26 @@
-def application(environ, start_response):
-    print(environ['PATH_INFO'])
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b'<h1>Hello1, web!</h1>']y
-    
+# num = 1
+# while num <= 9:
+# 	col = 1
+# 	while col <= num:
+# 		print('%d * %d = %d' % (col,num,col * num), end="  ")
+# 		col += 1
+# 	num += 1
+# 	print("")
+
+
+def surface(obj='name'):
+    nums = list(range(1, 10))
+    for i in nums:
+        col = 1
+        while col <= i:
+            print('%d * %d = %d' % (col, i, col*i), end="  ")
+            col += 1
+        print('')
+    return nums
+
+
+def print_line(chare='*', nums=50):
+    print(chare * nums)
+
+
+string = "中文"
